904293146c9eac3156961a61e6126b8496c5fbf1
Juan Murua Olalde <juan@susosise.es>
2023-02-07 12:36:47 +0100

    completar muestra de solucion a ProcesandoListas

--- ProcesandoListas/muestras_de_posibles_soluciones/java/Main.java
+++ ProcesandoListas/muestras_de_posibles_soluciones/java/Main.java
@@ -7,21 +7,68 @@ public class Main {
         Integer[] arrayOriginal = {34, 76, 295, 17, 861, 5, 95, 127, 3, 99, 26, 49, 117, 532, 15, 88, 2, 31};
         java.util.ArrayList<Integer> listaOriginal = new java.util.ArrayList<>();
         java.util.Collections.addAll(listaOriginal, arrayOriginal);
+        System.out.println();
         System.out.println("La lista original: " + listaOriginal.toString());
+
 
         java.util.ArrayList<Integer> listaDuplicada = new java.util.ArrayList<>();
         for (Integer unNumero : listaOriginal) {
             listaDuplicada.add(unNumero * 2);
         }
+        System.out.println();
         System.out.println("Multiplicando sus elementos por 2: " + listaDuplicada.toString());
+
 
         Integer suma = 0;
         for (Integer unNumero : listaOriginal) {
             suma = suma + unNumero;
         }
-        System.out.format("Los elementos de la lista original suman %,d" , suma);
+        System.out.println();
+        System.out.format("Los elementos de la lista original suman %,d \n" , suma);
 
 
+        java.util.ArrayList<Integer> soloImpares = new java.util.ArrayList<>();
+        for (Integer unNumero : listaOriginal) {
+            if(unNumero % 2 > 0) {
+                soloImpares.add(unNumero);
+            }
+        }
+        System.out.println();
+        System.out.println("Los impares de la lista son: " + soloImpares.toString());
+
+
+        java.util.ArrayList<Double> listaIncrementada = new java.util.ArrayList<>();
+        for (Integer unNumero : listaOriginal) {
+            listaIncrementada.add(unNumero + (unNumero * 0.30));
+        }
+        System.out.println();
+        System.out.println("Incrementando sus elementos un 30%: " + listaIncrementada.toString());
+
+
+        System.out.println();
+        java.util.ArrayList<MedicionMetereologica> mediciones = new java.util.ArrayList<>();
+        mediciones.add(new MedicionMetereologica(java.time.LocalDate.of(2023, 2, 14), 15.3, 4.6, 68, 45, 0.0));
+        mediciones.add(new MedicionMetereologica(java.time.LocalDate.of(2023, 2, 15), 18.2, 3.5, 78, 54, 2.5));
+        mediciones.add(new MedicionMetereologica(java.time.LocalDate.of(2023, 2, 16), 19.8, 2.6, 47, 41, 0.0));
+        mediciones.add(new MedicionMetereologica(java.time.LocalDate.of(2023, 2, 17), 17.3, 1.9, 52, 35, 7.5));
+        mediciones.add(new MedicionMetereologica(java.time.LocalDate.of(2023, 2, 18), 12.9, 3.7, 64, 43, 9.0));
+        System.out.println();
+        System.out.println("Con estos datos de partida:");
+        Double sumaDeMaximas = 0.0;
+        Double sumaDeMinimas = 0.0;
+        for (MedicionMetereologica medicion : mediciones) {
+            System.out.println();
+            System.out.println(medicion.toString());
+            sumaDeMaximas += medicion.getTemperatura_max_celsius();
+            sumaDeMinimas += medicion.getTemperatura_min_celsius();
+        }
+        Double temperaturaMediaMax = sumaDeMaximas / mediciones.size();
+        Double temperaturaMediaMin = sumaDeMinimas / mediciones.size();
+        System.out.println();
+        System.out.format("Nos da una temperatura media máxima de %.2f °C y mínima de %.2f °C",
+                          temperaturaMediaMax, temperaturaMediaMin);
+        System.out.println();
+        System.out.println();
 
     }
     
---
+++ ProcesandoListas/muestras_de_posibles_soluciones/java/MedicionMetereologica.java
@@ -0,0 +1,58 @@
+package ProcesandoListas.muestras_de_posibles_soluciones.java;
+
+public class MedicionMetereologica {
+    java.time.LocalDate fecha;
+    Double temperatura_max_celsius;
+    Double temperatura_min_celsius;
+    Integer humedad_max_porcentaje;
+    Integer humedad_min_porcentaje;
+    Double lluvia_litrospormetrocuadrado;
+    
+    public MedicionMetereologica(java.time.LocalDate fecha, 
+                                 Double temperatura_max_celsius, Double temperatura_min_celsius,
+                                 Integer humedad_max_porcentaje, Integer humedad_min_porcentaje, 
+                                 Double lluvia_litrospormetrocuadrado) {
+        this.fecha = fecha;
+        this.temperatura_max_celsius = temperatura_max_celsius;
+        this.temperatura_min_celsius = temperatura_min_celsius;
+        this.humedad_max_porcentaje = humedad_max_porcentaje;
+        this.humedad_min_porcentaje = humedad_min_porcentaje;
+        this.lluvia_litrospormetrocuadrado = lluvia_litrospormetrocuadrado;
+    }
+
+    public java.time.LocalDate getFecha() {
+        return fecha;
+    }
+
+    public Double getTemperatura_max_celsius() {
+        return temperatura_max_celsius;
+    }
+
+    public Double getTemperatura_min_celsius() {
+        return temperatura_min_celsius;
+    }
+
+    public Integer getHumedad_max_porcentaje() {
+        return humedad_max_porcentaje;
+    }
+
+    public Integer getHumedad_min_porcentaje() {
+        return humedad_min_porcentaje;
+    }
+
+    public Double getLluvia_litrospormetrocuadrado() {
+        return lluvia_litrospormetrocuadrado;
+    }
+
+    @Override
+    public String toString() {
+        java.time.format.DateTimeFormatter formateador = java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "Mediciones del día " + fecha.format(formateador) + "\n"
+                + String.format("Temperatura:  max = %.2f °C y min = %.2f °C", temperatura_max_celsius, temperatura_min_celsius) + "\n"  
+                + String.format("Humedad: max = %d %% y min = %d %%", humedad_max_porcentaje, humedad_min_porcentaje) + "\n"
+                + String.format("Precipitación: %.1f l/m2", lluvia_litrospormetrocuadrado);
+    }
+
+    
+
+}
