952afe445fefc1bf6d15b8e48bde8c7a37c650a1
Juan Murua Olalde <juan@susosise.es>
2023-08-18 14:16:37 +0200

    Corregir el ejemplo de posible solución orientada a objeto para Calculo
    Me habia alrevesado innecesariamente. Intentando implementar aspectos que solo enturbiaban la solución. Creo que ahora está más claro el uso básico de objetos como "agrupadores" de datos.
    
    A la hora de implementar más unidades de medida y sus respectivas conversiones. Es decir, al ilustrar el uso de objetos como gestores y "protectores" de datos. Es cuando habrá que complicar la solución...

--- OrientacionAObjeto/Calculo/Objeto/Magnitud.py
+++ OrientacionAObjeto/Calculo/Objeto/Magnitud.py
@@ -1,30 +1,27 @@
 import math
 
+
 class Magnitud:
 
-    def __init__(self, valor = float("nan"), unidadDeMedida = "sinasignar"):
+    def __init__(self, valor=float("nan"), unidadDeMedida="sinasignar"):
         self._valor = float(valor)
         self._unidadDeMedida = unidadDeMedida
 
     def getValor(self):
         return self._valor
-    def setValor(self, nuevoValor):
-        self._valor = float(nuevoValor)
 
     def getUnidadDeMedida(self):
         return self._unidadDeMedida
-    def setUnidadDeMedida(self, nuevaUnidadDeMedida):
-        self._unidadDeMedida = nuevaUnidadDeMedida
 
     def __str__(self):
-        return str(self._valor) + " " +  self._unidadDeMedida
-    
+        return str(self._valor) + " " + self._unidadDeMedida
 
     def ConvertirmeALaUnidadDeMedida(self, nuevaUnidadDeMedida):
         if nuevaUnidadDeMedida != self._unidadDeMedida:
             if nuevaUnidadDeMedida == "m" and self._unidadDeMedida == "cm":
                 self._valor = self._valor / 100
                 self._unidadDeMedida = "cm"
-        # como por ahora no vamos a convertir nada más, no nos hace falta nada más sofisticado...
-
-
+            # como por ahora no vamos a convertir nada más, no nos hace falta nada más sofisticado...
+            elif True:
+                raise ValueError(
+                    f"No está contemplada la conversión de {self._unidadDeMedida} a {nuevaUnidadDeMedida}")
--- OrientacionAObjeto/Calculo/Objeto/calculo.py
+++ OrientacionAObjeto/Calculo/Objeto/calculo.py
@@ -1,32 +1,35 @@
+from Magnitud import Magnitud
 from ParametrosDeEntrada import ParametrosDeEntrada
 from ParametrosDeSalida import ParametrosDeSalida
 from Calculador import Calculador
+
 
 def PedirDatosAlUsuario():
     parametros = ParametrosDeEntrada()
     print()
     print("Por favor, introduce los datos con los que hacer el cálculo.")
-    parametros.velocidadDelViento.setValor(float(input("Velocidad del viento (en m/s): ")))
-    parametros.velocidadDelViento.setUnidadDeMedida("m/s")
-    parametros.unLadoDeLaVela.setValor(float(input("Longitud de un lado de la vela (rectangular) (en cm): ")))
-    parametros.unLadoDeLaVela.setUnidadDeMedida("cm")
-    parametros.otroLadoDeLaVela.setValor(float(input("Longitud del otro lado de la vela (en cm): ")))
-    parametros.otroLadoDeLaVela.setUnidadDeMedida("cm")
+    parametros.velocidadDelViento = Magnitud(
+        float(input("Velocidad del viento (en m/s): ")), "m/s")
+    parametros.unLadoDeLaVela = Magnitud(
+        float(input("Longitud de un lado de la vela (rectangular) (en cm): ")), "cm")
+    parametros.otroLadoDeLaVela = Magnitud(
+        float(input("Longitud del otro lado de la vela (en cm): ")), "cm")
     aplicarRefuerzos_texto = input("¿Se van a aplicar refuerzos? (si o no): ")
     if aplicarRefuerzos_texto.upper() == "SI" or aplicarRefuerzos_texto.upper() == "S":
         parametros.aplicarRefuerzos = True
     elif aplicarRefuerzos_texto.upper() == "NO" or aplicarRefuerzos_texto.upper() == "N":
         parametros.aplicarRefuerzos = False
     else:
-        print(f"Al responder [{aplicarRefuerzos_texto}], se considera que NO se aplicarán refuerzos.")
+        print(
+            f"Al responder [{aplicarRefuerzos_texto}], se considera que NO se aplicarán refuerzos.")
         parametros.aplicarRefuerzos = False
-    parametros.densidadDeParticulas.setValor(float(input("Densidad de particulas (p/m3): ")))
-    parametros.densidadDeParticulas.setUnidadDeMedida("p/m3")
+    parametros.densidadDeParticulas = Magnitud(
+        float(input("Densidad de particulas (p/m3): ")), "p/m3")
     return parametros
 
-calculador = Calculador()
+
 entradas = PedirDatosAlUsuario()
-resultados = calculador.calcular(entradas)
+resultados = Calculador().calcular(entradas)
 
 print()
 print(entradas)
